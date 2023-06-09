fd6b2987145617bb0546ae92de122e00172b1419
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-06-09 19:41:05 +0200

    enable category selection

--- src/app/components/store/store.component.ts
+++ src/app/components/store/store.component.ts
@@ -9,14 +9,20 @@ import { ProductRepository } from 'src/app/model/product.repository';
 })
 export class StoreComponent  {
 
+  selectedCategory: string | undefined;
+
   constructor(private repository: ProductRepository) { }
 
   get products(): Product[]{
-    return this.repository.getProducts();
+    return this.repository.getProducts(this.selectedCategory);
   }
 
   get categories(): string[]{
     return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string){
+    this.selectedCategory = newCategory;
   }
 
 }
