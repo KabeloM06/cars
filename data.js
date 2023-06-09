1461b1f61f75f296833b69631ac41ac289da1c2a
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-06-09 16:45:52 +0200

    placeholder content set

---
+++ data.js
@@ -0,0 +1,12 @@
+module.exports = function(){
+    return{
+        products: [
+            {id: 1, name: "Toyota RunX", category: "Hatchback", description: "A gold hatch with four doors", price: 250000},
+            {id: 2, name: "Nissan NP-200", category: "Single Cab bakkie", description: "A single cab bakkie with two doors", price: 300000},
+            {id: 3, name: "Mazda CX-5", category: "SUV", description: "A green SUV for a large family", price: 650000},
+            {id: 4, name: "Ford Focus", category: "Hatchback", description: "A red hatch with four doors", price: 350000},
+            {id: 5, name: "VW Polo", category: "Hatchback", description: "A white hatch with four doors", price: 550000},
+        ],
+        orders: []
+    }
+}
