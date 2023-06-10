8cb2dab8370f5a959645ee72b2c9bc3e625a5fce
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-06-10 08:33:25 +0200

    basic navigation to cart

--- src/app/components/store/store.component.ts
+++ src/app/components/store/store.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Cart } from 'src/app/model/cart.model';
 import { Product } from 'src/app/model/product.model';
 import { ProductRepository } from 'src/app/model/product.repository';
@@ -15,7 +16,10 @@ export class StoreComponent  {
   productsPerPage = 4;
   selectedPage = 1;
 
-  constructor(private repository: ProductRepository, private cart: Cart) { }
+  constructor(private repository: ProductRepository,
+    private cart: Cart,
+    private router: Router
+    ) { }
 
   get products(): Product[]{
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
@@ -53,6 +57,7 @@ export class StoreComponent  {
 
   addProductToCart(product: Product){
     this.cart.addLine(product);
+    this.router.navigateByUrl("/cart");
   }
 
 }
