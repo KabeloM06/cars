82feb0905e04d4b1ff33f033edc70961bd499881
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-06-16 12:47:13 +0200

    add and edit products

--- src/app/admin/product-editor/product-editor.component.ts
+++ src/app/admin/product-editor/product-editor.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/model/product.model';
+import { ProductRepository } from 'src/app/model/product.repository';
 
 @Component({
   selector: 'app-product-editor',
@@ -7,7 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductEditorComponent {
 
-  constructor() { }
+  editing: boolean = false;
+  product: Product = new Product();
+
+  constructor(private repository: ProductRepository,
+              private router: Router,
+              activeRoute: ActivatedRoute) { 
+
+                this.editing = activeRoute.snapshot.params["mode"] == "edit";
+                if(this.editing){
+                  Object.assign(this.product,
+                                repository.getProduct(activeRoute.snapshot.params["id"]));
+                }
+              }
+
+              save(){
+                this.repository.saveProduct(this.product);
+                this.router.navigateByUrl("/admin/main/products");
+              }
 
   ngOnInit(): void {
   }
