2ac670905463321438c6c08ca9f6fae642e983c9
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-06-16 13:17:49 +0200

    order table implimented

--- src/app/admin/order-table/order-table.component.ts
+++ src/app/admin/order-table/order-table.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, IterableDiffer, IterableDiffers, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Order } from 'src/app/model/order.model';
+import { OrderRepository } from 'src/app/model/order.repository';
 
 @Component({
   selector: 'app-order-table',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderTableComponent {
 
-  constructor() { }
+  colsAndRows: string[] = ['name', 'postalCode', 'cart_p', 'cart_q', 'buttons'];
 
-  ngOnInit(): void {
-  }
+  dataSource = new MatTableDataSource<Order>(this.repository.getOrders());
+  differ: IterableDiffer<Order>;
+
+  constructor(private repository: OrderRepository,
+              differs: IterableDiffers) { 
+                this.differ = differs.find(this.repository.getOrders()).create();
+                this.dataSource.filter = "true";
+                this.dataSource.filterPredicate = (order, include) => {
+                  return !order.shipped || include.toString() == "true"
+                };
+              }
+
+              get includeShipped(): boolean {
+                return this.dataSource.filter == "true";
+              }
+              set includeShipped(include: boolean) {
+                this.dataSource.filter = include.toString()
+              }
+              toggleShipped(order: Order) {
+                order.shipped = !order.shipped;
+                this.repository.updateOrder(order);
+              }
+              delete(id: number) {
+                this.repository.deleteOrder(id);
+              }
+              ngDoCheck() {
+                let changes = this.differ?.diff(this.repository.getOrders());
+                if (changes != null) {
+                  this.dataSource.data = this.repository.getOrders();
+                }
+              }
 
 }
