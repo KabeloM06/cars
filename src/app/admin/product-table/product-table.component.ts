bc96cade95073992671e24dca3dbb1b2a76fd87a
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-06-16 11:52:29 +0200

    add pagination in product admin page

--- src/app/admin/product-table/product-table.component.ts
+++ src/app/admin/product-table/product-table.component.ts
@@ -1,4 +1,5 @@
-import { Component, IterableDiffer, IterableDiffers } from '@angular/core';
+import { Component, IterableDiffer, IterableDiffers, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Product } from 'src/app/model/product.model';
 import { ProductRepository } from 'src/app/model/product.repository';
@@ -9,9 +10,12 @@ import { ProductRepository } from 'src/app/model/product.repository';
   styleUrls: ['./product-table.component.css']
 })
 export class ProductTableComponent {
-  colsAndRows: string[] = ['id', 'name', 'category', 'price', 'button'];
+  colsAndRows: string[] = ['id', 'name', 'category', 'price', 'buttons'];
   dataSource = new MatTableDataSource<Product>(this.repository.getProducts());
   differ : IterableDiffer<Product>;
+
+  @ViewChild(MatPaginator)
+  paginator? : MatPaginator
 
   constructor( private repository: ProductRepository, differs: IterableDiffers) {
     this.differ = differs.find(this.repository.getProducts()).create();
@@ -21,6 +25,12 @@ export class ProductTableComponent {
     let changes = this.differ?.diff(this.repository.getProducts());
     if (changes != null) {
       this.dataSource.data = this.repository.getProducts();
+    }
+  }
+
+  ngAfterViewInit(){
+    if (this.paginator){
+      this.dataSource.paginator = this.paginator;
     }
   }
 
