87ee2ddc30868086e93e3c0d3eda5291645fa8f0
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-06-16 08:27:06 +0200

    display products in product admin page

--- src/app/admin/product-table/product-table.component.ts
+++ src/app/admin/product-table/product-table.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, IterableDiffer, IterableDiffers } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Product } from 'src/app/model/product.model';
+import { ProductRepository } from 'src/app/model/product.repository';
 
 @Component({
   selector: 'app-product-table',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-table.component.css']
 })
 export class ProductTableComponent {
+  colsAndRows: string[] = ['id', 'name', 'category', 'price', 'button'];
+  dataSource = new MatTableDataSource<Product>(this.repository.getProducts());
+  differ : IterableDiffer<Product>;
 
-  constructor() { }
+  constructor( private repository: ProductRepository, differs: IterableDiffers) {
+    this.differ = differs.find(this.repository.getProducts()).create();
+   }
 
-  ngOnInit(): void {
+  ngDoCheck(){
+    let changes = this.differ?.diff(this.repository.getProducts());
+    if (changes != null) {
+      this.dataSource.data = this.repository.getProducts();
+    }
   }
 
+  deleteProduct(id:number){
+    this.repository.deleteProduct(id);
+  }
 }
